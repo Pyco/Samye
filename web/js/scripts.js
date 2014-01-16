4be59348be6ee85cb23fb65154cfdb3eb96ca918
Pyco <py.cotty@gmail.com>
2014-01-16 14:29:09 +0100

    Transitions Events

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -1,14 +1,26 @@
 $(document).ready(function() {
 	
+	$('#contentRows .id').hide();
+	$('#resume a#button').hide();
 	
-
+	$('#resume a#button').click(function(){
+		$(this).parent("#resume").fadeOut(300);
+		$('#insideRight').fadeIn(750).removeClass("hidden");
+	});
     
-
+	$('a.detailEvt').click(function(){		
+		return false;				
+	},resumeEvtTab);
 	//testAjax();
 
 });
 
 function resumeEvt(eventId) {
+		
+	$("#resume").fadeIn(300, function() {				
+		$('#resume a#button').fadeIn(300);
+	});
+	
 	var tab = "";
 	
 	
@@ -19,38 +31,90 @@ function resumeEvt(eventId) {
 		dataType: "JSON",
 		success : function(msg) {
 			
-			;
-			
-			//dateDeb = new Date(msg[0].date, "mm/dd/yyyy");
-			
-			
-			//tab += "<div id='resume'>";
-			tab += "<h2>Evénement :"+msg[0].libelle+"</h2>";
-			tab += "<table><tr><td></td><td></td></tr><tr><td>Date : </td><td class='contentRow'>"+msg[0].date+"</td></tr>";
-			tab += "<tr><td>Heure : </td><td class='contentRow'>"+msg[0].heureDeb+" - "+msg[0].heureFin+"</td></tr>";
-			tab += "<tr><td>Lieu : </td><td class='contentRow'>"+msg[0].lieu+"</td></tr>";
-			tab += "<tr><td>Catégorie : </td><td class='contentRow'>"+msg[0].categorie+"</td></tr>";
-			tab += "<tr><td>Statut : </td><td class='contentRow'>"+msg[0].status+"</td></tr>";
-			tab += "<tr><td>Participation : </td><td class='contentRow'>"+msg[0].participation+"€</td></tr>";
-			tab += "<tr><td>Description : </td><td class='contentRow'>"+msg[0].description+"</td></tr>";
-			tab += "<tr><td>Auteur : </td><td class='contentRow'>"+msg[0].auteur+"</td></tr></table>";
-			//tab += "</div>";
+			tab += "<h2>Evénement : "+msg[0].libelle+"</h2>";
+			tab += "<div id='list'>";
+			tab += "<table><tr><td></td><td></td></tr><tr><td><strong>Date : </strong></td><td class='contentRow'>"+msg[0].date+"</td></tr>";
+			tab += "<tr><td><strong>Heure :  </strong></td><td class='contentRow'>"+msg[0].heureDeb+" - "+msg[0].heureFin+"</td></tr>";
+			tab += "<tr><td><strong>Lieu :  </strong></td><td class='contentRow'>"+msg[0].lieu+"</td></tr>";
+			tab += "<tr><td><strong>Catégorie : </strong> </td><td class='contentRow'>"+msg[0].categorie+"</td></tr>";
+			tab += "<tr><td><strong>Statut :  </strong></td><td class='contentRow'>"+msg[0].status+"</td></tr>";
+			tab += "<tr><td><strong>Participation :  </strong></td><td class='contentRow'>"+msg[0].participation+"€</td></tr>";
+			tab += "<tr><td><strong>Description :  </strong></td><td class='contentRow'>"+msg[0].description+"</td></tr>";
+			tab += "<tr><td><strong>Auteur :  </strong></td><td class='contentRow'>"+msg[0].auteur+"</td></tr></table>";
+			tab += "</div>";
 		
 			
 			$('#resume #contentRows').append(tab);
-			
-		
-		
-		
-			   
-			/*$.each(msg, function(i, item) {
-				
-
-			});*/
-			//callback(tab);
-			
 		}
 	});
+}
+
+
+function resumeEvtTab() {
+	
+	var recId = $('.detailEvt').attr("data");
+	console.log(recId);
+	var tab = "";
+	
+	
+	
+	$.ajax({
+		type : "POST",
+		url : "/samye/web/app_dev.php/allEvents/"+recId,
+		data : "id="+recId,
+		dataType: "JSON",
+		success : function(msg) {
+			if($('#insideRight').hasClass("hidden")) {
+				$('#resume').fadeOut(1000, function() {
+					$('#resume #contentRows').html("");
+				
+					tab += "<h2>Evénement : "+msg[0].libelle+"</h2>";
+					tab += "<div id='list'>";
+					tab += "<table><tr><td></td><td></td></tr><tr><td><strong>Date : </strong></td><td class='contentRow'>"+msg[0].date+"</td></tr>";
+					tab += "<tr><td><strong>Heure :  </strong></td><td class='contentRow'>"+msg[0].heureDeb+" - "+msg[0].heureFin+"</td></tr>";
+					tab += "<tr><td><strong>Lieu :  </strong></td><td class='contentRow'>"+msg[0].lieu+"</td></tr>";
+					tab += "<tr><td><strong>Catégorie : </strong> </td><td class='contentRow'>"+msg[0].categorie+"</td></tr>";
+					tab += "<tr><td><strong>Statut :  </strong></td><td class='contentRow'>"+msg[0].status+"</td></tr>";
+					tab += "<tr><td><strong>Participation :  </strong></td><td class='contentRow'>"+msg[0].participation+"€</td></tr>";
+					tab += "<tr><td><strong>Description :  </strong></td><td class='contentRow'>"+msg[0].description+"</td></tr>";
+					tab += "<tr><td><strong>Auteur :  </strong></td><td class='contentRow'>"+msg[0].auteur+"</td></tr></table>";
+					tab += "</div>";
+					//tab += '<a id="button">Retour</a>';
+					
+					$('#resume #contentRows').append(tab);
+					$('#resume a#button').fadeOut(2000);
+				});
+				
+			} else {
+				$('#insideRight').fadeOut(300).addClass("hidden");
+				$("#resume").fadeIn(750, function() {
+					//$('#insideRight').hide();
+					$('#resume #contentRows').html("");
+					
+					tab += "<h2>Evénement : "+msg[0].libelle+"</h2>";
+					tab += "<div id='list'>";
+					tab += "<table><tr><td></td><td></td></tr><tr><td><strong>Date : </strong></td><td class='contentRow'>"+msg[0].date+"</td></tr>";
+					tab += "<tr><td><strong>Heure :  </strong></td><td class='contentRow'>"+msg[0].heureDeb+" - "+msg[0].heureFin+"</td></tr>";
+					tab += "<tr><td><strong>Lieu :  </strong></td><td class='contentRow'>"+msg[0].lieu+"</td></tr>";
+					tab += "<tr><td><strong>Catégorie : </strong> </td><td class='contentRow'>"+msg[0].categorie+"</td></tr>";
+					tab += "<tr><td><strong>Statut :  </strong></td><td class='contentRow'>"+msg[0].status+"</td></tr>";
+					tab += "<tr><td><strong>Participation :  </strong></td><td class='contentRow'>"+msg[0].participation+"€</td></tr>";
+					tab += "<tr><td><strong>Description :  </strong></td><td class='contentRow'>"+msg[0].description+"</td></tr>";
+					tab += "<tr><td><strong>Auteur :  </strong></td><td class='contentRow'>"+msg[0].auteur+"</td></tr></table>";
+					tab += "</div>";
+					//tab += '<a id="button">Retour</a>';
+					
+					$('#resume #contentRows').append(tab);
+					$('#resume a#button').fadeIn(800);
+				});
+			}
+		}
+	});
+	
+	/*$.post('url', {id:"data"}, function(msg) {
+		
+	});*/
+	
 }
 
 /*****************************************/
